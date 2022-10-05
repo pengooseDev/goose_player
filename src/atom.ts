925563511489ead874b3cc6c25d34bfbd9959f02
pengoosedev <73521518+pengoosedev@users.noreply.github.com>
2022-10-06 07:37:06 +0900

    feat: add title Regexp

--- src/atom.ts
+++ src/atom.ts
@@ -1,9 +1,30 @@
 import { atom } from "recoil";
 import { v1 } from "uuid";
 
-export type IAxiosAtom = object[];
-
-export const axiosAtom = atom<IAxiosAtom>({
+export const axiosAtom = atom<axiosData[]>({
     key: `axiosAtom/${v1()}`,
     default: [],
 });
+
+export interface axiosData {
+    videoRenderer: {
+        channelThumbnailSupportedRenderers: {
+            channelThumbnailWithLinkRenderer: {
+                navigationEndpoint: {
+                    canonicalBaseUrl: string;
+                };
+            };
+        };
+
+        title: {
+            accessibility: { accessibilityData: { label: string } };
+        };
+
+        thumbnail: { thumbnails: IThumbnails[] };
+        videoId: string;
+    };
+}
+
+interface IThumbnails {
+    url: string;
+}
--- src/components/Data.tsx
+++ src/components/Data.tsx
@@ -1,18 +1,51 @@
 import { axiosAtom } from "../atom";
 import { useRecoilState } from "recoil";
 import styled from "styled-components";
-import { IAxiosAtom } from "../atom";
+import { axiosData } from "../atom";
+
+const dataTrimmer = (axiosData: axiosData) => {
+    //ChannelUrl
+    const info =
+        axiosData.videoRenderer?.channelThumbnailSupportedRenderers
+            .channelThumbnailWithLinkRenderer;
+    const channelId = info?.navigationEndpoint.canonicalBaseUrl;
+    const channelUrl = `https://www.youtube.com/${channelId}`;
+
+    //thumbnail
+    const thumbnail = axiosData.videoRenderer?.thumbnail?.thumbnails[0].url;
+
+    //Title
+    const titleData =
+        axiosData.videoRenderer?.title.accessibility.accessibilityData.label;
+
+    const titleRegex = /^.*? (?=게시자: )/g; //텍스트의 처음부터 "게시자"의 전방탐색까지.
+    const title = titleRegex.exec(titleData);
+
+    //ID
+    const id = axiosData.videoRenderer?.videoId;
+    console.log(axiosData);
+    return { title, id, channelUrl, thumbnail };
+};
 
 const Data = () => {
-    const [axiosData, setAxiosData] = useRecoilState(axiosAtom);
-    console.log(axiosData);
+    const [axiosData, setAxiosData] = useRecoilState<axiosData[]>(axiosAtom);
     return (
         <div>
             <h1>data</h1>
             <div>
-                {axiosData.map((v, i) => (
-                    <div key={i}>1</div>
-                ))}
+                {axiosData.map((v, i) => {
+                    const videoData = dataTrimmer(v);
+                    const { title, id, channelUrl, thumbnail } = videoData;
+                    if (!title || !id || !channelUrl || !thumbnail) return null;
+                    return (
+                        <div key={i}>
+                            <h4>{title}</h4>
+                            <div>id : {id}</div>
+                            <div>channel : {channelUrl}</div>
+                            <div>thumbnail : {thumbnail}</div>
+                        </div>
+                    );
+                })}
             </div>
         </div>
     );
