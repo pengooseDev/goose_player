485be374ac2347e102f3a38b075408d46460e915
pengoosedev <73521518+pengoosedev@users.noreply.github.com>
2023-02-01 22:05:11 +0900

    feat: Add Search Animation

--- src/components/search/Search.tsx
+++ src/components/search/Search.tsx
@@ -44,18 +44,22 @@ const Search = () => {
                   <Exit />
                 </Title>
                 <SearchBar />
-                {!axiosData.length && !isLoading ? (
-                  <SearchFilter>
-                    <Image src={searchGooseImgage} />
-                  </SearchFilter>
-                ) : null}
-                {isLoading ? (
-                  <LoadingFilter>
-                    <Loading>
-                      <LoadingGif>
-                        <Image src={loadingGIF} />
-                      </LoadingGif>
-                    </Loading>
+                {!axiosData.length ? (
+                  <LoadingFilter isLoading={isLoading}>
+                    <AnimatePresence>
+                      {isLoading ? (
+                        <Loading>
+                          <LoadingGif
+                            variants={loadingVar}
+                            initial="initial"
+                            animate="animate"
+                            exit="exit"
+                          >
+                            <Image src={loadingGIF} />
+                          </LoadingGif>
+                        </Loading>
+                      ) : null}
+                    </AnimatePresence>
                     <Image src={searchGooseImgage} />
                   </LoadingFilter>
                 ) : (
@@ -108,36 +112,40 @@ const containerVar = {
   },
 };
 
-const SearchFilter = styled(motion.div)`
-  background: rgba(12, 12, 12, 0.56);
-  display: flex;
-  align-items: center;
-  justify-content: center;
+interface FilterProps {
+  isLoading: boolean;
+}
 
-  border-radius: 5px;
-  padding-left: 50px;
-
-  img {
-    filter: grayscale(100%) brightness(0.25) invert(100%) blur(0px)
-      drop-shadow(0px 0px 5px bisque);
-  }
-`;
-
-const LoadingFilter = styled(motion.div)`
+const LoadingFilter = styled(motion.div)<FilterProps>`
   position: relative;
   display: flex;
   align-items: center;
   justify-content: center;
   border-radius: 5px;
   padding-left: 50px;
+  background: ${(props) =>
+    props.isLoading ? 'rgba(12, 12, 12, 0.85)' : 'rgba(12, 12, 12, 0.5)'};
+  transition: ease-in-out 0.25s;
+  height: 100%;
+  overflow: hidden;
 
   img {
-    filter: grayscale(100%) brightness(0.25) invert(0%) blur(0px)
-      drop-shadow(0px 0px 5px bisque);
+    transition: ease-in-out 0.25s;
+    object-fit: cover;
+    filter: ${(props) =>
+      props.isLoading
+        ? 'grayscale(100%) brightness(0.25) invert(100%) blur(0px) drop-shadow(5px 5px 10px bisque)'
+        : 'grayscale(100%) brightness(0.25) invert(0%) blur(0px) drop-shadow(0px 0px 2px black)'};
   }
 `;
 
-const LoadingGif = styled.div`
+const loadingVar = {
+  initial: { opacity: 0 },
+  animate: { opacity: 1, transition: { duration: 0.25 } },
+  exit: { opacity: 0, transition: { duration: 0.25 } },
+};
+
+const LoadingGif = styled(motion.div)`
   width: 250px;
   height: 250px;
 `;
