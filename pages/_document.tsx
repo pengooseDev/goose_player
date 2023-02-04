56a5da6c5403d5b939e75df6d0dd72ed32a0b03e
pengoosedev <73521518+pengoosedev@users.noreply.github.com>
2023-02-04 16:02:53 +0900

    feat: Set SSR config with Styled-components

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,7 +1,32 @@
 import React from 'react';
 import NextDocument, { Head, Html, Main, NextScript } from 'next/document';
+import { ServerStyleSheet } from 'styled-components';
 
-class Document extends NextDocument {
+export default class MyDocument extends NextDocument {
+  static async getInitialProps(ctx: any) {
+    const sheet = new ServerStyleSheet();
+    const originalRenderPage = ctx.renderPage;
+    try {
+      ctx.renderPage = () =>
+        originalRenderPage({
+          enhanceApp: (App: any) => (props: any) =>
+            sheet.collectStyles(<App {...props} />),
+        });
+      const initialProps = await NextDocument.getInitialProps(ctx);
+      return {
+        ...initialProps,
+        styles: (
+          <>
+            {initialProps.styles}
+            {sheet.getStyleElement()}
+          </>
+        ),
+      };
+    } finally {
+      sheet.seal();
+    }
+  }
+
   render() {
     return (
       <Html>
@@ -25,5 +50,3 @@ class Document extends NextDocument {
     );
   }
 }
-
-export default Document;
